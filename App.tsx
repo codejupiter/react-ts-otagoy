ea65aa45e9032c950bcb608a0f5f3cd9c4b772e4
codejupiter <119028679+codejupiter@users.noreply.github.com>
2022-12-01 17:28:02 -0500

    display random numbers

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
 import './style.css';
 
+const nums = [];
+const randomNumber = (start: number, end: number) => {
+  for(let i = 0 ; i < 10 ; ++ i)
+    nums.push(Math.floor(Math.random() * (end - start)));
+}
+randomNumber(0,40);
+
+const numItems = nums.map(num => (
+  <li>{num}</li>
+))
+
 export default function App() {
+  
+  
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <ul>{numItems}</ul>
   );
 }
